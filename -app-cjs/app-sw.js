f78f56d870794f5cf4971ed0699ffab8c394e23e
James A. Howard <jahoward11@gmail.com>
2021-06-09 18:24:58 -0700

    Update app-sw.js

--- -app-cjs/app-sw.js
+++ -app-cjs/app-sw.js
@@ -24,33 +24,6 @@ self.addEventListener('install', e => {
   );
 });
 
-self.addEventListener('fetch', e => {
-  //console.log("[Service Worker] Fetching resource: " + e.request.url);
-  e.respondWith(
-    !caches ? fetch(e.request)
-    : caches.match(e.request).then(rsp1 => {
-      let tsfetch = Date.now();
-      if ( rsp1.ok && ( !navigator.onLine
-      || !/\/a00\/-app-cjs\/[\w.-]+\??$/.test(e.request.url) )) {
-        return rsp1;
-      } else {
-        return fetch(e.request).then(rsp2 =>
-          !rsp2.ok || e.request.method !== 'GET' || /\?rev=/.test(e.request.url)
-          || !/\/a00\/[\w/.-]+\??$/.test(e.request.url)
-          ? rsp1.ok && rsp1 || rsp2
-          : caches.open(cacheName).then(cache => {
-              console.log( "[Service Worker] Caching new resource: " + e.request.url
-                + "\n  (Time elapsed since SW install: "
-                + ((tsfetch - tstampsw) / (60 * 1000)) + " min)" );
-              cache.put(e.request, rsp2.clone());
-              return rsp2;
-            })
-        );
-      }
-    }).catch(() => fetch(e.request))
-  );
-});
-
 self.addEventListener('activate', e => {
   console.log( "[Service Worker] Activating new cache: " + cacheName
     + "\n  (Time elapsed since SW install: "
@@ -59,5 +32,32 @@ self.addEventListener('activate', e => {
     caches.keys().then( keyList => Promise.all( keyList.map( key =>
       (cacheKeeplist || []).indexOf(key) > -1 || caches.delete(key)
     )))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  //console.log("[Service Worker] Fetching resource: " + e.request.url);
+  let reqPrc = (rsp1 = {}) => {
+    if ( rsp1.ok && ( !navigator.onLine
+    || !/\/a00\/-app-cjs\/[\w.-]+\??$/.test(e.request.url) )) {
+      return rsp1;
+    } else {
+      return fetch(e.request).then(rsp2 =>
+        !rsp2.ok || e.request.method !== 'GET' || /\?rev=/.test(e.request.url)
+        || !/\/a00\/[\w/.-]+\??$/.test(e.request.url)
+        ? rsp1.ok && rsp1 || rsp2
+        : caches.open(cacheName).then(cache => {
+            console.log( "[Service Worker] Caching new resource: " + e.request.url
+              + "\n  (Time elapsed since SW install: "
+              + ((Date.now() - tstampsw) / (60 * 1000)) + " min)" );
+            cache.put(e.request, rsp2.clone());
+            return rsp2;
+          })
+      );
+    }
+  };
+  e.respondWith(
+    !caches ? fetch(e.request)
+    : caches.match(e.request).then(reqPrc).catch(reqPrc).catch(() => fetch(e.request))
   );
 });
