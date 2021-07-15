e379843c9392670dee3bb3e1fd639252cbe2eeef
James A. Howard <jahoward11@gmail.com>
2021-07-15 16:04:29 -0700

    Add files via upload

--- -app-cjs/cjs-srvc4.mjs
+++ -app-cjs/cjs-srvc4.mjs
@@ -7,14 +7,14 @@ const apbiology1 = `
 
 // *Metric Prefixes*
  // __Factor	Prefix	Symbol__
- //  10⁹	giga	G
- //  10⁶	mega	M
- //  10³	kilo	k
- // 10⁻¹	deci	d
- // 10⁻²	centi	c
- // 10⁻³	milli	m
- // 10⁻⁶	micro	*μ*
- // 10⁻⁹	nano	n
+ //   10⁹	giga	G
+ //   10⁶	mega	M
+ //   10³	kilo	k
+ //  10⁻¹	deci	d
+ //  10⁻²	centi	c
+ //  10⁻³	milli	m
+ //  10⁻⁶	micro	*μ*
+ //  10⁻⁹	nano	n
  // 10⁻¹²	pico	p
 
 // *Statistical Analysis and Probability*
@@ -39,7 +39,7 @@ const apbiology1 = `
 
 // __Standard Deviation__
  //
- // *s* = √   ∑(*x*~i~ - *x̅*)² ⟋ &nbsp; *n* - 1  &nbsp;
+ // *s* = √   ∑(*x*~i~ - *x̅*)² ⟋ ⬚͏ *n* - 1  ⬚͏
  //
 // __Standard Error of the Mean__
  //
@@ -47,13 +47,13 @@ const apbiology1 = `
  //
 // __Chi-Square__
  //
- // *χ*² = ∑   (*o* - *e*)² ⟋ &nbsp;  *e*   &nbsp;
+ // *χ*² = ∑   (*o* - *e*)² ⟋ ⬚͏  *e*   ⬚͏
  //
 // __Chi-Square Table__
- // *p*    | Degrees of Freedom
- // v̲a̲l̲ ̲ |  ̲ ̲1̲ ̲ ̲  ̲ ̲2̲ ̲ ̲  ̲ ̲3̲ ̲ ̲  ̲ ̲4̲ ̲ ̲  ̲ ̲5̲ ̲ ̲  ̲ ̲6̲ ̲ ̲  ̲ ̲7̲ ̲ ̲  ̲ ̲8̲ ̲ ̲ 
- // 0.05 |  3.84  5.99  7.81  9.49 11.07 12.59 14.07 15.51 
- // 0.01 |  6.63  9.21 11.34 13.28 15.09 16.81 18.48 20.09 
+   // *p*   | Degrees of Freedom
+ // v̲a̲l̲u̲e̲ |  ̲ ̲1̲ ̲ ̲  ̲ ̲2̲ ̲ ̲  ̲ ̲3̲ ̲ ̲  ̲ ̲4̲ ̲ ̲  ̲ ̲5̲ ̲ ̲  ̲ ̲6̲ ̲ ̲  ̲ ̲7̲ ̲ ̲  ̲ ̲8̲ ̲ ̲ 
+  // 0.05 |  3.84  5.99  7.81  9.49 11.07 12.59 14.07 15.51 
+  // 0.01 |  6.63  9.21 11.34 13.28 15.09 16.81 18.48 20.09 
 
  // Degrees of freedom are equal to the number of
  // distinct possible outcomes minus one.
@@ -69,7 +69,7 @@ const apbiology1 = `
  // *p* | frequency of allele 1 in a population
  // *q* | frequency of allele 2 in a population
 
- // *p*² + 2&zwj;*pq* + *q*² = 1
+ // *p*² + 2┊͏*pq* + *q*² = 1
  // *p* + *q* = l
 //
 `;
