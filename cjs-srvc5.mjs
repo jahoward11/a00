fa9a2c11832890387b234a635b179d5c0d8a42be
James A. Howard <jahoward11@gmail.com>
2021-08-05 03:50:07 -0700

    Add files via upload

---
+++ cjs-srvc5.mjs
@@ -0,0 +1,61 @@
+/* JavaScript module */
+
+const groupname = "statistics";
+
+const analysis1 = `/*
+__Statistical Analysis Tools__
+
+*Z-score*
+ *z* | how many standard deviations away from the mean
+
+ *z*  =  *x - μ* ⟋ *⬚͏ σ ⬚͏*	      =  ~data point - mean~ ⟋ ^standard deviation^
+
+
+*Correlation Coefficient*
+
+ *r*  =  ⬚͏ 1 ⟋ *n* - 1	     ∑ *z~xi~ z~yi~*  =  ⬚͏ 1 ⟋ *n* - 1	∑   [  *x~i~* - *x̅* ⟋ ⬚͏ *s~x~* ⬚͏      ] [  *y~i~* - *y̅* ⟋ ⬚͏ *s~y~* ⬚͏      ] 
+
+
+/*
+*Normal Distribution, Probability Density Function (PDF)*
+
+ *p*(*x*)   =   ⬚͏1 ⟋ *σ*√2*π*	 exp[  -(*x - μ*)² ⟋ ⬚͏  2*σ*²  ⬚͏	      ] 
+
+	=   ⬚͏  1 ⟋ *σ*√2*π*ℯ^*z*²^
+
+ *φ*(*x*)   =    ⬚͏ 1 ⟋ √2*π*ℯ^*z*²^		PDF for a standard n.d.
+
+*/
+ μ = 0
+ σ = 1
+
+ x = 1
+ z = (x - μ) / σ
+
+ Math.exp(z**2)
+ φ_z = 1 / (2 * Math.PI * Math.exp(z**2))**0.5
+
+
+/*
+*Normal Distribution, Cumulative Density Function (CDF)*
+
+ *Φ*(*z*)   =   ∫ *φ*(*z*) *dz*	CDF for a standard n.d.
+
+*/
+ b0 = 0.2316419
+ b1 = 0.319381530
+ b2 = -0.356563782
+ b3 = 1.781477937
+ b4 = -1.821255978
+ b5 = 1.330274429
+
+ t = 1 / (1 + b0 * z)
+
+ Φ_z = 1 - φ_z * (b1*t + b2*t**2 + b3*t**3 + b4*t**4 + b5*t**5)
+
+ 2 * Φ_z - 1	// coverage, area between *μ - nσ* and *μ + nσ*
+//`;
+
+export {
+  groupname, analysis1
+};
