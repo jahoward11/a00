e93728bf3368b0cb2d22cb61aa200b2ec35668e9
James A. Howard <jahoward11@gmail.com>
2021-08-05 05:57:41 -0700

    Add files via upload

--- -app-cjs/cjs-srvc5.mjs
+++ -app-cjs/cjs-srvc5.mjs
@@ -6,23 +6,24 @@ const analysis1 = `/*
 __Statistical Analysis Tools__
 
 *Z-score*
- *z* | how many standard deviations away from the mean
+ *z* | how many standard deviations is a value away from the mean?
 
  *z*  =  *x - μ* ⟋ *⬚͏ σ ⬚͏*	      =  ~data point - mean~ ⟋ ^standard deviation^
 
 
 *Correlation Coefficient*
+ *r* | how closely aligned are plot points to linear regression line?
 
  *r*  =  ⬚͏ 1 ⟋ *n* - 1	     ∑ *z~xi~ z~yi~*  =  ⬚͏ 1 ⟋ *n* - 1	∑   [  *x~i~* - *x̅* ⟋ ⬚͏ *s~x~* ⬚͏      ] [  *y~i~* - *y̅* ⟋ ⬚͏ *s~y~* ⬚͏      ] 
 
 
 *Normal Distribution, Probability Density Function (PDF)*
 
- *p*(*x*)   =   ⬚͏1 ⟋ *σ*√2*π*	 exp[  -(*x - μ*)² ⟋ ⬚͏  2*σ*²  ⬚͏	      ] 
+ *p*(*x*)  =   ⬚͏1 ⟋ *σ*√2*π*	exp[  -(*x - μ*)² ⟋ ⬚͏  2*σ*²  ⬚͏	     ] 
 
-	=   ⬚͏  1 ⟋ *σ*√2*π*ℯ^*z*²^
+       =   ⬚͏  1 ⟋ *σ*√2*π*ℯ^*z*²^
 
- *φ*(*x*)   =    ⬚͏ 1 ⟋ √2*π*ℯ^*z*²^		PDF for a standard n.d.
+ *φ*(*x*)  =    ⬚͏ 1 ⟋ √2*π*ℯ^*z*²^		PDF for a standard n.d.
 
 */
  μ = 0
@@ -31,14 +32,13 @@ __Statistical Analysis Tools__
  x = 1
  z = (x - μ) / σ
 
- Math.exp(z**2)
  φ_z = 1 / (2 * Math.PI * Math.exp(z**2))**0.5
 
 
 /*
 *Normal Distribution, Cumulative Density Function (CDF)*
 
- *Φ*(*z*)   =   ∫ *φ*(*z*) *dz*	CDF for a standard n.d.
+ *Φ*(*z*)  =  ∫ *φ*(*z*) *dz*	CDF for a standard n.d.
 
 */
  b0 = 0.2316419
@@ -52,6 +52,14 @@ __Statistical Analysis Tools__
 
  Φ_z = 1 - φ_z * (b1*t + b2*t**2 + b3*t**3 + b4*t**4 + b5*t**5)
 
+/*
+*The 68-95-99.7 (empirical) rule, or the 3-sigma rule*
+
+ 68.27% | probability of a position w/i &pm;1 std. dev.
+ 95.45% | probability of a position w/i &pm;2 std. devs.
+ 99.73% | probability of a position w/i &pm;3 std. devs.
+
+*/
  2 * Φ_z - 1	// coverage, area between *μ - nσ* and *μ + nσ*
 //`;
 
