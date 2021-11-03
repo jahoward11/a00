b94a1f546ec4e6a16142ae7e5a9c0cd4b5ab7c71
James A. Howard <jahoward11@gmail.com>
2021-11-03 16:47:57 -0700

    Add files via upload

---
+++ -res-js/jstat-tdist.js
@@ -0,0 +1,400 @@
+// from jstat.js
+
+(function () {
+'use strict';
+window.jStat = {};
+
+function isNumber(num) {
+  return (typeof num === 'number') ? num - num === 0 : false;
+}
+
+jStat._random_fn = Math.random;
+
+// Generate a rows x cols matrix according to the supplied function.
+jStat.create = function create(rows, cols, func) {
+  var res = new Array(rows);
+  var i, j;
+  if (isFunction(cols)) {
+    func = cols;
+    cols = rows;
+  }
+  for (i = 0; i < rows; i++) {
+    res[i] = new Array(cols);
+    for (j = 0; j < cols; j++)
+      res[i][j] = func(i, j);
+  }
+  return res;
+};
+
+function retZero() { return 0; }
+
+// Generate a rows x cols matrix of zeros.
+jStat.zeros = function zeros(rows, cols) {
+  if (!isNumber(cols))
+    cols = rows;
+  return jStat.create(rows, cols, retZero);
+};
+
+// sum of an array
+jStat.sum = function sum(arr) {
+  var sum = 0;
+  var i = arr.length;
+  while (--i >= 0)
+    sum += arr[i];
+  return sum;
+};
+
+// mean value of an array
+jStat.mean = function mean(arr) {
+  return jStat.sum(arr) / arr.length;
+};
+
+// sum of squared errors of prediction (SSE)
+jStat.sumsqerr = function sumsqerr(arr) {
+  var mean = jStat.mean(arr);
+  var sum = 0;
+  var i = arr.length;
+  var tmp;
+  while (--i >= 0) {
+    tmp = arr[i] - mean;
+    sum += tmp * tmp;
+  }
+  return sum;
+};
+
+// variance of an array
+// flag = true indicates sample instead of population
+jStat.variance = function variance(arr, flag) {
+  return jStat.sumsqerr(arr) / (arr.length - (flag ? 1 : 0));
+};
+
+// standard deviation of an array
+// flag = true indicates sample instead of population
+jStat.stdev = function stdev(arr, flag) {
+  return Math.sqrt(jStat.variance(arr, flag));
+};
+
+// gamma of x
+jStat.gammafn = function gammafn(x) {
+  var p = [-1.716185138865495, 24.76565080557592, -379.80425647094563,
+           629.3311553128184, 866.9662027904133, -31451.272968848367,
+           -36144.413418691176, 66456.14382024054
+  ];
+  var q = [-30.8402300119739, 315.35062697960416, -1015.1563674902192,
+           -3107.771671572311, 22538.118420980151, 4755.8462775278811,
+           -134659.9598649693, -115132.2596755535];
+  var fact = false;
+  var n = 0;
+  var xden = 0;
+  var xnum = 0;
+  var y = x;
+  var i, z, yi, res;
+  if (x > 171.6243769536076) {
+    return Infinity;
+  }
+  if (y <= 0) {
+    res = y % 1 + 3.6e-16;
+    if (res) {
+      fact = (!(y & 1) ? 1 : -1) * Math.PI / Math.sin(Math.PI * res);
+      y = 1 - y;
+    } else {
+      return Infinity;
+    }
+  }
+  yi = y;
+  if (y < 1) {
+    z = y++;
+  } else {
+    z = (y -= n = (y | 0) - 1) - 1;
+  }
+  for (i = 0; i < 8; ++i) {
+    xnum = (xnum + p[i]) * z;
+    xden = xden * z + q[i];
+  }
+  res = xnum / xden + 1;
+  if (yi < y) {
+    res /= yi;
+  } else if (yi > y) {
+    for (i = 0; i < n; ++i) {
+      res *= y;
+      y++;
+    }
+  }
+  if (fact) {
+    res = fact / res;
+  }
+  return res;
+};
+
+// Log-gamma function
+jStat.gammaln = function gammaln(x) {
+  var j = 0;
+  var cof = [
+    76.18009172947146, -86.50532032941677, 24.01409824083091,
+    -1.231739572450155, 0.1208650973866179e-2, -0.5395239384953e-5
+  ];
+  var ser = 1.000000000190015;
+  var xx, y, tmp;
+  tmp = (y = xx = x) + 5.5;
+  tmp -= (xx + 0.5) * Math.log(tmp);
+  for (; j < 6; j++)
+    ser += cof[j] / ++y;
+  return Math.log(2.5066282746310005 * ser / xx) - tmp;
+};
+
+// natural logarithm of beta function
+jStat.betaln = function betaln(x, y) {
+  return jStat.gammaln(x) + jStat.gammaln(y) - jStat.gammaln(x + y);
+};
+
+// beta function
+jStat.betafn = function betafn(x, y) {
+  // ensure arguments are positive
+  if (x <= 0 || y <= 0)
+    return undefined;
+  // make sure x + y doesn't exceed the upper limit of usable values
+  return (x + y > 170)
+      ? Math.exp(jStat.betaln(x, y))
+      : jStat.gammafn(x) * jStat.gammafn(y) / jStat.gammafn(x + y);
+};
+
+// Evaluates the continued fraction for incomplete beta function by modified
+// Lentz's method.
+jStat.betacf = function betacf(x, a, b) {
+  var fpmin = 1e-30;
+  var m = 1;
+  var qab = a + b;
+  var qap = a + 1;
+  var qam = a - 1;
+  var c = 1;
+  var d = 1 - qab * x / qap;
+  var m2, aa, del, h;
+
+  // These q's will be used in factors that occur in the coefficients
+  if (Math.abs(d) < fpmin)
+    d = fpmin;
+  d = 1 / d;
+  h = d;
+
+  for (; m <= 100; m++) {
+    m2 = 2 * m;
+    aa = m * (b - m) * x / ((qam + m2) * (a + m2));
+    // One step (the even one) of the recurrence
+    d = 1 + aa * d;
+    if (Math.abs(d) < fpmin)
+      d = fpmin;
+    c = 1 + aa / c;
+    if (Math.abs(c) < fpmin)
+      c = fpmin;
+    d = 1 / d;
+    h *= d * c;
+    aa = -(a + m) * (qab + m) * x / ((a + m2) * (qap + m2));
+    // Next step of the recurrence (the odd one)
+    d = 1 + aa * d;
+    if (Math.abs(d) < fpmin)
+      d = fpmin;
+    c = 1 + aa / c;
+    if (Math.abs(c) < fpmin)
+      c = fpmin;
+    d = 1 / d;
+    del = d * c;
+    h *= del;
+    if (Math.abs(del - 1.0) < 3e-7)
+      break;
+  }
+
+  return h;
+};
+
+// Returns the incomplete beta function I_x(a,b)
+jStat.ibeta = function ibeta(x, a, b) {
+  // Factors in front of the continued fraction.
+  var bt = (x === 0 || x === 1) ?  0 :
+    Math.exp(jStat.gammaln(a + b) - jStat.gammaln(a) -
+             jStat.gammaln(b) + a * Math.log(x) + b *
+             Math.log(1 - x));
+  if (x < 0 || x > 1)
+    return false;
+  if (x < (a + 1) / (a + b + 2))
+    // Use continued fraction directly.
+    return bt * jStat.betacf(x, a, b) / a;
+  // else use continued fraction after making the symmetry transformation.
+  return 1 - bt * jStat.betacf(1 - x, b, a) / b;
+};
+
+// Returns the inverse of the incomplete beta function
+jStat.ibetainv = function ibetainv(p, a, b) {
+  var EPS = 1e-8;
+  var a1 = a - 1;
+  var b1 = b - 1;
+  var j = 0;
+  var lna, lnb, pp, t, u, err, x, al, h, w, afac;
+  if (p <= 0)
+    return 0;
+  if (p >= 1)
+    return 1;
+  if (a >= 1 && b >= 1) {
+    pp = (p < 0.5) ? p : 1 - p;
+    t = Math.sqrt(-2 * Math.log(pp));
+    x = (2.30753 + t * 0.27061) / (1 + t* (0.99229 + t * 0.04481)) - t;
+    if (p < 0.5)
+      x = -x;
+    al = (x * x - 3) / 6;
+    h = 2 / (1 / (2 * a - 1)  + 1 / (2 * b - 1));
+    w = (x * Math.sqrt(al + h) / h) - (1 / (2 * b - 1) - 1 / (2 * a - 1)) *
+        (al + 5 / 6 - 2 / (3 * h));
+    x = a / (a + b * Math.exp(2 * w));
+  } else {
+    lna = Math.log(a / (a + b));
+    lnb = Math.log(b / (a + b));
+    t = Math.exp(a * lna) / a;
+    u = Math.exp(b * lnb) / b;
+    w = t + u;
+    if (p < t / w)
+      x = Math.pow(a * w * p, 1 / a);
+    else
+      x = 1 - Math.pow(b * w * (1 - p), 1 / b);
+  }
+  afac = -jStat.gammaln(a) - jStat.gammaln(b) + jStat.gammaln(a + b);
+  for(; j < 10; j++) {
+    if (x === 0 || x === 1)
+      return x;
+    err = jStat.ibeta(x, a, b) - p;
+    t = Math.exp(a1 * Math.log(x) + b1 * Math.log(1 - x) + afac);
+    u = err / t;
+    x -= (t = u / (1 - 0.5 * Math.min(1, u * (a1 / x - b1 / (1 - x)))));
+    if (x <= 0)
+      x = 0.5 * (x + t);
+    if (x >= 1)
+      x = 0.5 * (x + t + 1);
+    if (Math.abs(t) < EPS * x && j > 0)
+      break;
+  }
+  return x;
+};
+
+// Returns a normal deviate (mu=0, sigma=1).
+// If n and m are specified it returns a object of normal deviates.
+jStat.randn = function randn(n, m) {
+  var u, v, x, y, q;
+  if (!m)
+    m = n;
+  if (n)
+    return jStat.create(n, m, function() { return jStat.randn(); });
+  do {
+    u = jStat._random_fn();
+    v = 1.7156 * (jStat._random_fn() - 0.5);
+    x = u - 0.449871;
+    y = Math.abs(v) + 0.386595;
+    q = x * x + y * (0.19600 * y - 0.25472 * x);
+  } while (q > 0.27597 && (q > 0.27846 || v * v > -4 * Math.log(u) * u * u));
+  return v / u;
+};
+
+// Returns a gamma deviate by the method of Marsaglia and Tsang.
+jStat.randg = function randg(shape, n, m) {
+  var oalph = shape;
+  var a1, a2, u, v, x, mat;
+  if (!m)
+    m = n;
+  if (!shape)
+    shape = 1;
+  if (n) {
+    mat = jStat.zeros(n,m);
+    mat.alter(function() { return jStat.randg(shape); });
+    return mat;
+  }
+  if (shape < 1)
+    shape += 1;
+  a1 = shape - 1 / 3;
+  a2 = 1 / Math.sqrt(9 * a1);
+  do {
+    do {
+      x = jStat.randn();
+      v = 1 + a2 * x;
+    } while(v <= 0);
+    v = v * v * v;
+    u = jStat._random_fn();
+  } while(u > 1 - 0.331 * Math.pow(x, 4) &&
+          Math.log(u) > 0.5 * x*x + a1 * (1 - v + Math.log(v)));
+  // alpha > 1
+  if (shape == oalph)
+    return a1 * v;
+  // alpha < 1
+  do {
+    u = jStat._random_fn();
+  } while(u === 0);
+  return Math.pow(u, 1 / oalph) * a1 * v;
+};
+
+// extend studentt function with static methods
+jStat.studentt = {
+  pdf: function pdf(x, dof) {
+    dof = dof > 1e100 ? 1e100 : dof;
+    return (1/(Math.sqrt(dof) * jStat.betafn(0.5, dof/2))) *
+        Math.pow(1 + ((x * x) / dof), -((dof + 1) / 2));
+  },
+  cdf: function cdf(x, dof) {
+    var dof2 = dof / 2;
+    return jStat.ibeta((x + Math.sqrt(x * x + dof)) /
+                       (2 * Math.sqrt(x * x + dof)), dof2, dof2);
+  },
+  inv: function(p, dof) {
+    var x = jStat.ibetainv(2 * Math.min(p, 1 - p), 0.5 * dof, 0.5);
+    x = Math.sqrt(dof * (1 - x) / x);
+    return (p > 0.5) ? x : -x;
+  },
+  mean: function mean(dof) {
+    return (dof > 1) ? 0 : undefined;
+  },
+  median: function median(/*dof*/) {
+    return 0;
+  },
+  mode: function mode(/*dof*/) {
+    return 0;
+  },
+  sample: function sample(dof) {
+    return jStat.randn() * Math.sqrt(dof / (2 * jStat.randg(dof / 2)));
+  },
+  variance: function variance(dof) {
+    return (dof  > 2) ? dof / (dof - 2) : (dof > 1) ? Infinity : undefined;
+  }
+};
+
+// T Statistics
+jStat.tscore = function tscore() {
+  // 2 parameter lists
+  // (value, mean, sd, n)
+  // (value, array)
+  var args = Array.prototype.slice.call(arguments);
+  return (args.length === 4) ?
+    ((args[0] - args[1]) / (args[2] / Math.sqrt(args[3]))) :
+    ((args[0] - jStat.mean(args[1])) /
+     (jStat.stdev(args[1], true) / Math.sqrt(args[1].length)));
+};
+
+jStat.ttest = function ttest() {
+  // 3 different paramter lists:
+  // (value, mean, sd, n, sides)
+  // (tscore, n, sides)
+  // (value, array, sides)
+  var args = Array.prototype.slice.call(arguments);
+  var tscore;
+  if (args.length === 5) {
+    tscore = Math.abs(jStat.tscore(args[0], args[1], args[2], args[3]));
+    return (args[4] === 1) ?
+      (jStat.studentt.cdf(-tscore, args[3]-1)) :
+      (jStat.studentt.cdf(-tscore, args[3]-1)*2);
+  }
+  if (isNumber(args[1])) {
+    tscore = Math.abs(args[0]);
+    return (args[2] == 1) ?
+      (jStat.studentt.cdf(-tscore, args[1]-1)) :
+      (jStat.studentt.cdf(-tscore, args[1]-1) * 2);
+  }
+  tscore = Math.abs(jStat.tscore(args[0], args[1]))
+  return (args[2] == 1) ?
+    (jStat.studentt.cdf(-tscore, args[1].length-1)) :
+    (jStat.studentt.cdf(-tscore, args[1].length-1) * 2);
+};
+})();
