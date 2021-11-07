2688a4d8ad6f24498dd93cf990d74b00125a775d
James A. Howard <jahoward11@gmail.com>
2021-11-07 00:05:35 -0700

    Add files via upload

--- -res-js/jstat-tdist.js
+++ -res-js/jstat-tdist.js
@@ -74,62 +74,6 @@ jStat.stdev = function stdev(arr, flag) {
   return Math.sqrt(jStat.variance(arr, flag));
 };
 
-// Returns the error function erf(x)
-jStat.erf = function erf(x) {
-  var cof = [-1.3026537197817094, 6.4196979235649026e-1, 1.9476473204185836e-2,
-             -9.561514786808631e-3, -9.46595344482036e-4, 3.66839497852761e-4,
-             4.2523324806907e-5, -2.0278578112534e-5, -1.624290004647e-6,
-             1.303655835580e-6, 1.5626441722e-8, -8.5238095915e-8,
-             6.529054439e-9, 5.059343495e-9, -9.91364156e-10,
-             -2.27365122e-10, 9.6467911e-11, 2.394038e-12,
-             -6.886027e-12, 8.94487e-13, 3.13092e-13,
-             -1.12708e-13, 3.81e-16, 7.106e-15,
-             -1.523e-15, -9.4e-17, 1.21e-16,
-             -2.8e-17];
-  var j = cof.length - 1;
-  var isneg = false;
-  var d = 0;
-  var dd = 0;
-  var t, ty, tmp, res;
-  if (x < 0) {
-    x = -x;
-    isneg = true;
-  }
-  t = 2 / (2 + x);
-  ty = 4 * t - 2;
-  for(; j > 0; j--) {
-    tmp = d;
-    d = ty * d - dd + cof[j];
-    dd = tmp;
-  }
-  res = t * Math.exp(-x * x + 0.5 * (cof[0] + ty * d) - dd);
-  return isneg ? res - 1 : 1 - res;
-};
-
-// Returns the complmentary error function erfc(x)
-jStat.erfc = function erfc(x) {
-  return 1 - jStat.erf(x);
-};
-
-// Returns the inverse of the complementary error function
-jStat.erfcinv = function erfcinv(p) {
-  var j = 0;
-  var x, err, t, pp;
-  if (p >= 2)
-    return -100;
-  if (p <= 0)
-    return 100;
-  pp = (p < 1) ? p : 2 - p;
-  t = Math.sqrt(-2 * Math.log(pp / 2));
-  x = -0.70711 * ((2.30753 + t * 0.27061) /
-                  (1 + t * (0.99229 + t * 0.04481)) - t);
-  for (; j < 2; j++) {
-    err = jStat.erfc(x) - pp;
-    x += err / (1.12837916709551257 * Math.exp(-x * x) - x * err);
-  }
-  return (p < 1) ? x : -x;
-};
-
 // gamma of x
 jStat.gammafn = function gammafn(x) {
   var p = [-1.716185138865495, 24.76565080557592, -379.80425647094563,
@@ -196,6 +140,141 @@ jStat.gammaln = function gammaln(x) {
   for (; j < 6; j++)
     ser += cof[j] / ++y;
   return Math.log(2.5066282746310005 * ser / xx) - tmp;
+};
+
+// The lower regularized incomplete gamma function, usually written P(a,x)
+jStat.lowRegGamma = function lowRegGamma(a, x) {
+  var aln = jStat.gammaln(a);
+  var ap = a;
+  var sum = 1 / a;
+  var del = sum;
+  var b = x + 1 - a;
+  var c = 1 / 1.0e-30;
+  var d = 1 / b;
+  var h = d;
+  var i = 1;
+  // calculate maximum number of itterations required for a
+  var ITMAX = -~(Math.log((a >= 1) ? a : 1 / a) * 8.5 + a * 0.4 + 17);
+  var an;
+  if (x < 0 || a <= 0) {
+    return NaN;
+  } else if (x < a + 1) {
+    for (; i <= ITMAX; i++) {
+      sum += del *= x / ++ap;
+    }
+    return (sum * Math.exp(-x + a * Math.log(x) - (aln)));
+  }
+  for (; i <= ITMAX; i++) {
+    an = -i * (i - a);
+    b += 2;
+    d = an * d + b;
+    c = b + an / c;
+    d = 1 / d;
+    h *= d * c;
+  }
+  return (1 - h * Math.exp(-x + a * Math.log(x) - (aln)));
+};
+
+// Returns the inverse of the lower regularized inomplete gamma function
+jStat.gammapinv = function gammapinv(p, a) {
+  var j = 0;
+  var a1 = a - 1;
+  var EPS = 1e-8;
+  var gln = jStat.gammaln(a);
+  var x, err, t, u, pp, lna1, afac;
+  if (p >= 1)
+    return Math.max(100, a + 100 * Math.sqrt(a));
+  if (p <= 0)
+    return 0;
+  if (a > 1) {
+    lna1 = Math.log(a1);
+    afac = Math.exp(a1 * (lna1 - 1) - gln);
+    pp = (p < 0.5) ? p : 1 - p;
+    t = Math.sqrt(-2 * Math.log(pp));
+    x = (2.30753 + t * 0.27061) / (1 + t * (0.99229 + t * 0.04481)) - t;
+    if (p < 0.5)
+      x = -x;
+    x = Math.max(1e-3,
+                 a * Math.pow(1 - 1 / (9 * a) - x / (3 * Math.sqrt(a)), 3));
+  } else {
+    t = 1 - a * (0.253 + a * 0.12);
+    if (p < t)
+      x = Math.pow(p / t, 1 / a);
+    else
+      x = 1 - Math.log(1 - (p - t) / (1 - t));
+  }
+  for(; j < 12; j++) {
+    if (x <= 0)
+      return 0;
+    err = jStat.lowRegGamma(a, x) - p;
+    if (a > 1)
+      t = afac * Math.exp(-(x - a1) + a1 * (Math.log(x) - lna1));
+    else
+      t = Math.exp(-x + a1 * Math.log(x) - gln);
+    u = err / t;
+    x -= (t = u / (1 - 0.5 * Math.min(1, u * ((a - 1) / x - 1))));
+    if (x <= 0)
+      x = 0.5 * (x + t);
+    if (Math.abs(t) < EPS * x)
+      break;
+  }
+  return x;
+};
+
+// Returns the error function erf(x)
+jStat.erf = function erf(x) {
+  var cof = [-1.3026537197817094, 6.4196979235649026e-1, 1.9476473204185836e-2,
+             -9.561514786808631e-3, -9.46595344482036e-4, 3.66839497852761e-4,
+             4.2523324806907e-5, -2.0278578112534e-5, -1.624290004647e-6,
+             1.303655835580e-6, 1.5626441722e-8, -8.5238095915e-8,
+             6.529054439e-9, 5.059343495e-9, -9.91364156e-10,
+             -2.27365122e-10, 9.6467911e-11, 2.394038e-12,
+             -6.886027e-12, 8.94487e-13, 3.13092e-13,
+             -1.12708e-13, 3.81e-16, 7.106e-15,
+             -1.523e-15, -9.4e-17, 1.21e-16,
+             -2.8e-17];
+  var j = cof.length - 1;
+  var isneg = false;
+  var d = 0;
+  var dd = 0;
+  var t, ty, tmp, res;
+  if (x < 0) {
+    x = -x;
+    isneg = true;
+  }
+  t = 2 / (2 + x);
+  ty = 4 * t - 2;
+  for(; j > 0; j--) {
+    tmp = d;
+    d = ty * d - dd + cof[j];
+    dd = tmp;
+  }
+  res = t * Math.exp(-x * x + 0.5 * (cof[0] + ty * d) - dd);
+  return isneg ? res - 1 : 1 - res;
+};
+
+// Returns the complmentary error function erfc(x)
+jStat.erfc = function erfc(x) {
+  return 1 - jStat.erf(x);
+};
+
+// Returns the inverse of the complementary error function
+jStat.erfcinv = function erfcinv(p) {
+  var j = 0;
+  var x, err, t, pp;
+  if (p >= 2)
+    return -100;
+  if (p <= 0)
+    return 100;
+  pp = (p < 1) ? p : 2 - p;
+  t = Math.sqrt(-2 * Math.log(pp / 2));
+  x = -0.70711 * ((2.30753 + t * 0.27061) /
+                  (1 + t * (0.99229 + t * 0.04481)) - t);
+  for (; j < 2; j++) {
+    err = jStat.erfc(x) - pp;
+    x += err / (1.12837916709551257 * Math.exp(-x * x) - x * err);
+  }
+  return (p < 1) ? x : -x;
 };
 
 // natural logarithm of beta function
@@ -378,6 +457,41 @@ jStat.randg = function randg(shape, n, m) {
     u = jStat._random_fn();
   } while(u === 0);
   return Math.pow(u, 1 / oalph) * a1 * v;
+};
+
+// extend chisquare function with static methods
+jStat.chisquare = {
+  pdf: function pdf(x, dof) {
+    if (x < 0)
+      return 0;
+    return (x === 0 && dof === 2) ? 0.5 :
+        Math.exp((dof / 2 - 1) * Math.log(x) - x / 2 - (dof / 2) *
+                 Math.log(2) - jStat.gammaln(dof / 2));
+  },
+  cdf: function cdf(x, dof) {
+    if (x < 0)
+      return 0;
+    return jStat.lowRegGamma(dof / 2, x / 2);
+  },
+  inv: function(p, dof) {
+    return 2 * jStat.gammapinv(p, 0.5 * dof);
+  },
+  mean : function(dof) {
+    return dof;
+  },
+  // TODO: this is an approximation (is there a better way?)
+  median: function median(dof) {
+    return dof * Math.pow(1 - (2 / (9 * dof)), 3);
+  },
+  mode: function mode(dof) {
+    return (dof - 2 > 0) ? dof - 2 : 0;
+  },
+  sample: function sample(dof) {
+    return jStat.randg(dof / 2) * 2;
+  },
+  variance: function variance(dof) {
+    return 2 * dof;
+  }
 };
 
 // extend normal function with static methods
