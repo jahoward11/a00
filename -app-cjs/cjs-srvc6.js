66d01e626ccc691d71914a04bf8b17b349950b19
James A. Howard <jahoward11@gmail.com>
2021-12-16 15:33:16 -0800

    Add files via upload

--- -app-cjs/cjs-srvc6.js
+++ -app-cjs/cjs-srvc6.js
@@ -74,9 +74,9 @@ and repurposed as needed to build an original, standalone app.
       written within the parentheses.
 */
 
-ucLen = n => n * 0.9144  // converts a length in yards to meters
+ucLen = n => n * 12  // converts a length in feet to inches
 
-ucLen(100)  // calls function &amp; returns result of 100 * 0.9144
+ucLen(3)  // calls function &amp; returns result of 3 * 12
 
 /*
  3. __Modify the function__ to handle bi-directional conversions.
@@ -104,7 +104,7 @@ ucLen(3, "ft")   // calls function &amp; returns result of 3 * 12
       correct calculation formula.
 */
 
-ucLen = (n, u0, u1) => /^in/i.test(u0) && /^ft/i.test(u1) ? n => n / 12 : /^in/i.test(u0) && /^yd/i.test(u1) n => n / 36 : /^ft/i.test(u0) && /^in/i.test(u1) ? n => n * 12 : /^ft/i.test(u0) && /^yd/i.test(u1) ? n => n / 3 : /^yd/i.test(u0) && /^in/i.test(u1) ? n => n * 36 : /^yd/i.test(u0) && /^ft/i.test(u1) ? n => n * 3 : u0 === u1 ? n : "error"; ""
+ucLen = (n, u0, u1) => /^in/i.test(u0) && /^ft/i.test(u1) ? n => n / 12 : /^in/i.test(u0) && /^yd/i.test(u1) ? n => n / 36 : /^ft/i.test(u0) && /^in/i.test(u1) ? n => n * 12 : /^ft/i.test(u0) && /^yd/i.test(u1) ? n => n / 3 : /^yd/i.test(u0) && /^in/i.test(u1) ? n => n * 36 : /^yd/i.test(u0) && /^ft/i.test(u1) ? n => n * 3 : u0 === u1 ? n : "error"; ""
  // makes conversion between any lengths as
  // inches (in), feet (ft) or yards (yd)
 ucLen(72, "in", "yd")   // converts 72 inches to yards
