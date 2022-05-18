f10dd1edef9291a53c0c56142fdc540960fd9192
James A. Howard <jahoward11@gmail.com>
2022-05-18 16:01:57 -0700

    Add files via upload

--- -app-cjs/cmod3.js
+++ -app-cjs/cmod3.js
@@ -20,31 +20,31 @@ const homeloan = `
  // *dend* | ending date
 
 // __GIVEN VALUE (*key~0~*)__
- rate0 = 0.035 / 12	//yearly interest rate ÷ 12 ^mos^/~yr~
+ rate0 = 0.0525 / 12	//yearly interest rate ÷ 12 ^mos^/~yr~
  ppmt0 = 0
 
- pval0 = 250000
+ pval0 = 500000
 
  fval0 = 0
 
  nper0 = 30 * 12   	//number of years × 12 ^mos^/~yr~
- dbeg0 = new Date("2021-07-01")
+ dbeg0 = new Date("2022-07-01")
 
 // __CALCULATED VALUE (*key~1~*)__
- ppmt1 = rate0 * pval0 / [1 - Math.pow(1 + rate0, -nper0)]
+ ppmt1 = ppmt0 || rate0 * pval0 / [1 - Math.pow(1 + rate0, -nper0)]
  $ppmt1 = !nper0 ? "" : (Math.round(ppmt1 * 100) / 100).toFixed(2)
 
- pval1 = [(ppmt0 || $ppmt1) / rate0] * [1 - Math.pow(1 + rate0, -nper0)]
+ pval1 = pval0 || [$ppmt1 / rate0] * [1 - Math.pow(1 + rate0, -nper0)]
  $pval1 = !nper0 ? "" : Math.round(pval1).toFixed(2)
 
- fval1 = pval0 * Math.pow(1 + rate0, nper0) - (ppmt0 / rate0) * [Math.pow(1 + rate0, nper0) - 1]
- fint1 = (ppmt0 || $ppmt1) * nper0 - (pval0 || $pval1)
+ fval1 = fval0 || $pval1 * Math.pow(1 + rate0, nper0) - ($ppmt1 / rate0) * [Math.pow(1 + rate0, nper0) - 1]
+ fint1 = $ppmt1 * nper0 - ($pval1 - fval1)
  $fint1 = !nper0 ? "" : (Math.round(fint1 * 100) / 100).toFixed(2)
 
- nper1 = - Math.log(1 - rate0 * (pval0 || $pval1) / (ppmt0 || $ppmt1)) / Math.log(1 + rate0)
+ nper1 = -Math.log(1 - rate0 * $pval1 / $ppmt1) / Math.log(1 + rate0)
  $nper1 = nper0 || Math.round(nper1)
  nyrs1 = Math.round($nper1 * 10 / 12) / 10
- dend1 = new Date(new Date(dbeg0).getTime() + [(nper0 || $nper1) + 1] * 30.35 * 24 * 60 * 60 * 1000)
+ dend1 = new Date(new Date(dbeg0).getTime() + [$nper1 + 1] * 30.35 * 24 * 60 * 60 * 1000)
 
 // __QUICK-START GUIDE__
 // 1. To adjust the calculated *Monthly Payment* ($ppmt1)
